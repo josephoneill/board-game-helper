f5bf358492303d0364ea2257d4ad1f584a041707
Joseph O'Neill <devjoseph97@gmail.com>
2024-01-12 15:39:29 -0500

    Update entry point

--- app.vue
+++ app.vue
@@ -1,54 +1,58 @@
 <template>
-  <div class="lost-cities-container">
-    <LostCityExpedition
-      :expeditionType="ExpeditionType.YELLOW"
-      color="#fbbb23"
-      textColor="black"
-      @add-card="onAddCard"
-      @remove-card="onRemoveCard"
-    />
-    <LostCityExpedition
-      :expeditionType="ExpeditionType.WHITE"
-      color="#d2dcd9"
-      textColor="black"
-      @add-card="onAddCard"
-      @remove-card="onRemoveCard"
-    />
-    <LostCityExpedition
-      :expeditionType="ExpeditionType.BLUE"
-      color="#2b60a0"
-      textColor="white"
-      @add-card="onAddCard"
-      @remove-card="onRemoveCard"
-    />
-    <LostCityExpedition
-      :expeditionType="ExpeditionType.GREEN"
-      color="#37a31b"
-      textColor="white"
-      @add-card="onAddCard"
-      @remove-card="onRemoveCard"
-    />
-    <LostCityExpedition
-      :expeditionType="ExpeditionType.RED"
-      color="#db3e17"
-      @add-card="onAddCard"
-      @remove-card="onRemoveCard"
-    />
-    <LostCityExpedition
-      :expeditionType="ExpeditionType.PURPLE"
-      color="#524286"
-      @add-card="onAddCard"
-      @remove-card="onRemoveCard"
-    />
-    <p>Score: {{ score }}</p>
+  <div>
+    <Toolbar />
+    <div class="lost-cities-container">
+      <LostCityExpedition
+        :expeditionType="ExpeditionType.YELLOW"
+        color="#fbbb23"
+        textColor="black"
+        @add-card="onAddCard"
+        @remove-card="onRemoveCard"
+      />
+      <LostCityExpedition
+        :expeditionType="ExpeditionType.WHITE"
+        color="#d2dcd9"
+        textColor="black"
+        @add-card="onAddCard"
+        @remove-card="onRemoveCard"
+      />
+      <LostCityExpedition
+        :expeditionType="ExpeditionType.BLUE"
+        color="#2b60a0"
+        textColor="white"
+        @add-card="onAddCard"
+        @remove-card="onRemoveCard"
+      />
+      <LostCityExpedition
+        :expeditionType="ExpeditionType.GREEN"
+        color="#37a31b"
+        textColor="white"
+        @add-card="onAddCard"
+        @remove-card="onRemoveCard"
+      />
+      <LostCityExpedition
+        :expeditionType="ExpeditionType.RED"
+        color="#db3e17"
+        @add-card="onAddCard"
+        @remove-card="onRemoveCard"
+      />
+      <LostCityExpedition
+        :expeditionType="ExpeditionType.PURPLE"
+        color="#524286"
+        @add-card="onAddCard"
+        @remove-card="onRemoveCard"
+      />
+      <p>Score: {{ score }}</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import LostCitiesGameManager from './core/LostCities/LostCitiesGameManager';
-import { ExpeditionType } from './core/LostCities/LostCitiesExpedition';
-import type { AddCardEvent, RemoveCardEvent } from './core/LostCities/LostCitiesEvents';
-import LostCityExpedition from './components/LostCities/LostCityExpedition';
+import LostCitiesGameManager from '@core/LostCities/LostCitiesGameManager';
+import { ExpeditionType } from '@core/LostCities/LostCitiesExpedition';
+import type { AddCardEvent, RemoveCardEvent } from '@core/LostCities/LostCitiesEvents';
+import LostCityExpedition from '@components/LostCities/LostCityExpedition';
+import Toolbar from '@components/Toolbar';
 
 const score = ref(0);
 
@@ -70,9 +74,19 @@ function getScore() {
 </script>
 
 <style lang="scss">
+html {
+  background: #1d1d1d;
+  color: white;
+  font-family: Montserrat, 'Courier New', Courier, monospace;
+}
+
+body {
+  margin: 0;
+}
 .lost-cities-container {
   display: flex;
   align-items: center;
   flex-direction: column;
+  margin: 0 1rem;
 }
 </style>
