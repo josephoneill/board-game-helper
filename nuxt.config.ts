bf097bbec81ca769ef7134af711e6aaf4a1f8243
Joseph O'Neill <devjoseph97@gmail.com>
2024-01-12 15:39:49 -0500

    Add aliases for paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  alias: {
+		'@': './',
+    '@core': './core',
+		'@components': './components',
+		'@assets': './assets',
+		'@stores': './stores',
+	},
   devtools: { enabled: true },
   devServer: {
     host: "0.0.0.0",
-  }
+  },
+  modules: [
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Montserrat: true,
+      }
+    }]
+  ],
 })
