451396f4a26a494c4af7a0ec94783d350be5fa33
Joseph O'Neill <devjoseph97@gmail.com>
2024-01-12 15:40:24 -0500

    Add Toolbar

---
+++ components/Toolbar/Toolbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="toolbar">
+    <span class="toolbar-title">{{ title }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const title = ref('Lost Cities Calculator');
+</script>
+
+<style src="./Toolbar.scss" lang="scss" />
