df6f9de1831c07b75ef5363ccfcd030ce0f5eefe
Joseph O'Neill <devjoseph97@gmail.com>
2024-01-12 16:44:45 -0500

    Add global css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
+import { defineNuxtConfig } from 'nuxt/config';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   alias: {
-		'@': './',
-    '@core': './core',
-		'@components': './components',
-		'@assets': './assets',
-		'@stores': './stores',
+		'@': '/',
+    '@css': '/css',
+    '@core': '/core',
+		'@components': '/components',
+		'@assets': '/assets',
+		'@stores': '/stores',
 	},
+  css: ['@css/main.scss'],
   devtools: { enabled: true },
   devServer: {
     host: "0.0.0.0",
